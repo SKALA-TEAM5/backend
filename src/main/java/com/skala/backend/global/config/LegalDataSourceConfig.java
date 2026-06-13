f0febc6b0c79deeb83be5bf5e2d09f42b154f0c6
ss19801 <ss19801@naver.com>
2026-06-13 14:41:01 +0900

    feat: 법령 변경 이력 조회 API 추가 (GET /law-changes/recent)

---
+++ src/main/java/com/skala/backend/global/config/LegalDataSourceConfig.java
@@ -0,0 +1,44 @@
+package com.skala.backend.global.config;
+
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceProperties;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Primary;
+import org.springframework.jdbc.core.JdbcTemplate;
+
+import javax.sql.DataSource;
+
+@Configuration
+public class LegalDataSourceConfig {
+
+    @Bean
+    @Primary
+    @ConfigurationProperties(prefix = "spring.datasource")
+    public DataSourceProperties primaryDataSourceProperties() {
+        return new DataSourceProperties();
+    }
+
+    @Bean
+    @Primary
+    public DataSource primaryDataSource() {
+        return primaryDataSourceProperties().initializeDataSourceBuilder().build();
+    }
+
+    @Bean
+    @ConfigurationProperties(prefix = "spring.legal-datasource")
+    public DataSourceProperties legalDataSourceProperties() {
+        return new DataSourceProperties();
+    }
+
+    @Bean
+    public DataSource legalDataSource() {
+        return legalDataSourceProperties().initializeDataSourceBuilder().build();
+    }
+
+    @Bean
+    public JdbcTemplate legalJdbcTemplate(@Qualifier("legalDataSource") DataSource legalDataSource) {
+        return new JdbcTemplate(legalDataSource);
+    }
+}
